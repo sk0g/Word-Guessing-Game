cc6d009937d170d201d9588c10d3c64b19c02664
sk0g <tenderanger@gmail.com>
2017-03-12 15:45:13 +1000

    Create and populate list with words

--- solution.py
+++ solution.py
@@ -1,5 +1,8 @@
+# Import spam
+import random
+
 # Safely try to open a file, and print an error message if unsuccessful
-def FileOpen(name)
+def FileOpen(name):
     try:
         f = open(name, 'r')
         return f
@@ -16,11 +19,18 @@ def integer_in(minimum, maximum, statement = ''):
             print(statement)
             a = int(input())
             if a < minimum or a > maximum:
-                print('Improper value. Expected input was between' + str(minimum) ' and ' + str(maximum))
+                print('Improper value. Expected input was between' + str(minimum) + ' and ' + str(maximum))
         except ValueError:
             continue
     return a
 
-
+words = []
 f = FileOpen('dictionary.txt')
-guesses = integer_in(1, 100, 'Please enter how many guesses the computer will be allowed: ')
+guesses_num = integer_in(1, 100, 'Please enter how many guesses the computer should be allowed: ')
+print('The computer will be allowed to have ' + str(guesses_num) + ' guesses.')
+
+
+# Add all words to a list for easier access
+# Don't remove strip, or it will save the newlines to the list
+for word in f:
+    words.append(word.strip())
