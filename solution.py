f7538099ac72a3dc4c71ac5a01656cb6694e1d47
sk0g <tenderanger@gmail.com>
2017-05-05 20:49:20 +1000

    More work done
    Joel pls approve
    thanks

--- solution.py
+++ solution.py
@@ -1,8 +1,5 @@
-# Import spam
-import random
-
 # Safely try to open a file, and print an error message if unsuccessful
-def file_open(name):
+def FileOpen(name):
     try:
         f = open(name, 'r')
         return f
@@ -10,42 +7,38 @@ def file_open(name):
         print("Error: File does not exist, or is not called " + name)
         return 0
 
-# Bully user into answering with a "yes" or "no"
-def prompt_looper(a, b = "yes", c = "no"):
-    while 1:
-        res = input()
-        try:
-            res = res.lower()
-        except:
-            print('Please type in one of either ' + b + ' or ' + c)
-            continue
-        if res == b:
-            return True
-        elif res == c:
-            return False
 
-# Safely read an integer, and discard inputs outside the specified range, or of other data types
-def integer_in(minimum, maximum, statement = ''):
-    a = minimum - 1
-    # While loop keeps asking for another input if the input is not appropriate
-    while a < minimum:
+def integer_in(length_list, statement):
+    while True:
+        print(statement)
         try:
-            print(statement)
             a = int(input())
-            if a < minimum or a > maximum:
-                print('Improper value. Expected input was between' + str(minimum) + ' and ' + str(maximum))
         except ValueError:
-            continue
-    return a
+            print('Improper number, try again.')
+        if len(a) <= 0 or len(a) not in length_list:
+            print('No word found for that length, try again.')
+        else:
+            return a
 
-def play_game():
-    guesses_num = integer_in(1, 100, 'Please enter how many guesses the computer should be allowed: ')
-    print('The computer will be allowed to have ' + str(guesses_num) + ' guesses.')
-    running_total_flag = prompt_looper("Do you want to have a running total of the remaining words?")
 
-WORDS_LIST = []
-WORDS_LIST_FILE = file_open('dictionary.txt')
-# Add all words to a list for easier access
-# Don't remove strip, or it will save the newlines to the list
-for word in WORDS_LIST_FILE:
-    WORDS_LIST.append(word.strip())
+def generate_len_list(f):
+    length_list = []
+    for word in f:
+        f_length = len(f)
+        if f_length not in length_list:
+            length_list.append(f_length)
+    return length_list
+
+
+f = FileOpen('dictionary.txt')
+length_list = generate_len_list(f)
+
+replay = True
+while replay:
+    word_length = integer_in(length_list, "What should your word length be?")
+    word_hint = eval(input('Do you want to see the running total of remaining valid guesses?\
+                            1 for yes, 0 for no.'))
+    valid_words = [word for word in f if len(word) == word_length]
+
+    replay = eval(input('Would you like to play another game\
+                        1 for yes, 0 for no.'))
