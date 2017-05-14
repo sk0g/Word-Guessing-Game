4d5a8c9615fcb2ca0a66cdce58306bb404429f92
= <=>
2017-05-14 19:50:33 +1000

    V1.0 complete
    Fixed game replay logic
    Put main game loop in a function to make it more easy to call

--- solution.py
+++ solution.py
@@ -61,31 +61,35 @@ def get_word_lengths(word_list):
 
 
 def get_biggest_category_index(guess_char, word_list):
-    word_len = len(word_list[0]) + 1
-    occurences_list = [0] * word_len
+    word_len = len(word_list[0])
+    occurences_list = [0] * (word_len + 1)
     for word in word_list:
         num = word.find(guess_char)
         occurences_list[num] += 1
     most_common, highest_val = 0, 0
-    for i in range(word_len):  # Find the most common length
+    for i in range(word_len + 1):  # Find the most common length
         if occurences_list[i] > highest_val:
             highest_val = occurences_list[i]
             most_common = i
-    if most_common == word_len - 1:
-        most_common == -1
+    if most_common == word_len:
+        most_common = -1
     return most_common
 
 
-dictionary_file = file_open('dictionary.txt')
-word_list = file_process(dictionary_file)
-word_lengths = get_word_lengths(word_list)
+def get_biggest_category_wordlist(word_list, char, index):
+    words = [word for word in word_list if word.find(char) == index]
+    return words
 
-guesses_num = int(input('How many guesses would you like?\n'))
-replay = True
-while replay:
+
+def list_to_string(ls):
+    return("".join(ls))
+
+
+def game_process():
+    word_lengths = get_word_lengths(word_list)
+    guesses_num = int(input('How many guesses would you like?\n'))
     word_length = integer_in(word_lengths, "What should your word length be?")
-    word_hint = eval(input('Do you want to see the running total of remaining valid guesses? \
-                            1 for yes, 0 for no.\n'))
+    word_hint = eval(input('Do you want to see the running total of remaining valid guesses?\n1 for yes, 0 for no. '))
     valid_words = [word for word in word_list if len(word) == word_length]
     display_word = ["-"] * word_length
 
@@ -95,31 +99,40 @@ while replay:
     while guesses_num:
         if won:
             break
+        else:
+            print("You have", guesses_num, "guesses remaining. \n \n")
         char_guess = char_in(used_words_list)
         guessed_chars.append(char_guess)
         used_words_list.append(char_guess)
         index = get_biggest_category_index(char_guess, valid_words)
-        valid_words = [word for word in valid_words if word.find(guessed_chars) == index]
+        valid_words = get_biggest_category_wordlist(valid_words, char_guess, index)
         if 0 <= index < word_length:
             display_word[index] = char_guess
-            print("The letter was found at position number: ", index+1)
-            print("Your current word is: "".join(display_word")
-            print("You have guessed the following letters:", guessed_chars)
+            print("The letter was found at position number: ", index + 1)
         else:
             print("The letter you guessed wasn't in the word.")
             print("One less guess for you then.")
-            print('You have', guesses_num, 'guesses remaining.\n')
             guesses_num -= 1
+        print("You have guessed the following letters:", " ".join(guessed_chars))
         if word_hint:
-            print(len(valid_words))
+            print("Number of words remaining:", len(valid_words))
+        print(list_to_string(display_word))
         if "-" not in display_word:
             won = True
     if won:
         print("Well done, you found the word!")
-        print("".join(display_word))
+        print(list_to_string(display_word))
     else:
         print("Well, you didn't win... Might as well show you the word.")
         print(choice(valid_words))
 
+
+dictionary_file = file_open('dictionary.txt')
+word_list = file_process(dictionary_file)
+replay = True
+while replay:
+    game_process()
     replay = eval(input('Would you like to play another game\
-                        1 for yes, 0 for no.\n'))
+                        \n1 for yes, 0 for no. '))
+
+
